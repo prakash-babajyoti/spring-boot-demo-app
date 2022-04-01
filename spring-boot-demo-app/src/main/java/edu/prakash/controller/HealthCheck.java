21c09ffc2331c7b9c2d0f066548af847f639b102
Prakash Babajyoti <prakash.babajyoti@gmail.com>
2022-04-01 19:37:33 +0530

    application.properties takes priority over application.yml

---
+++ spring-boot-demo-app/src/main/java/edu/prakash/controller/HealthCheck.java
@@ -0,0 +1,13 @@
+package edu.prakash.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+
+	@GetMapping("/healthcheck")
+	public String healthCheck(){
+		return "Success";
+	}
+}
